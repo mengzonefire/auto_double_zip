17b619f158e4ab35eb8fe23dc65c11c1b2bfe009
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2021-09-29 14:40:54 +0800

    Update auto_7z.py

--- auto_7z.py
+++ auto_7z.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-import subprocess, traceback, os
-version = '1.0'
+import subprocess
+import traceback
+import os
+import sys
+version = '1.0.1'
 file_pw = ''    # 密码
 pw_file = '密码写在这里面.txt'
 input_path = '需要压缩的文件放这里'
@@ -9,30 +12,36 @@ output_path = '压缩完成的文件在这里'
 
 def get_pw():
     global file_pw
-    with open(pw_file, 'r', encoding='utf-8') as f:
-        file_pw = f.read().strip()
-    
+    if os.path.exists(pw_file):
+        with open(pw_file, 'r', encoding='utf-8') as f:
+            file_pw = f.read().strip()
+    else:
+        with open(pw_file, 'w') as f:
+            f.write()
+
 
 def get_files():
     dirs = os.listdir(input_path)
     if not os.path.exists(output_path):
         os.mkdir(output_path)
-    for i in range(len(dirs)):
-        command = 'HaoZipC a -t7z "../{2}/{0}(内层).7z" "../{1}/{0}" -mhe=on -mmt=on'\
-            .format(dirs[i], input_path, output_path)
-        command2 = 'HaoZipC a -t7z -p"{0}" "../{2}/{1}.7z" "../{2}/{1}(内层).7z" -mhe=on -mmt=on' \
-            .format(file_pw, dirs[i], output_path)
+    for file in dirs:
+        command = 'HaoZipC a -t7z "../{2}/{0}(内层).7z" "../{1}/{0}" -mhe=on -mmt=on'.format(
+            file, input_path, output_path)
+        command2 = 'HaoZipC a -t7z -p"{0}" "../{2}/{1}.7z" "../{2}/{1}(内层).7z" -mhe=on -mmt=on'.format(
+            file_pw, file, output_path)
         # 同路径下需要有压缩程序 HaoZipC
         subprocess.run(command, shell=True, cwd="./HaoZip")
         subprocess.run(command2, shell=True, cwd="./HaoZip")
-        os.remove('./{1}/{0}(内层).7z'.format(dirs[i], output_path))
+        os.remove('./{1}/{0}(内层).7z'.format(file, output_path))
 
 
 def main():
+    exe_path, exe_name = os.path.split(sys.argv[0])
+    if exe_path:
+        os.chdir(exe_path)  # 切换工作目录到程序根目录
     print('version: '+version)
     get_pw()
     get_files()
-    input('按任意键退出')
 
 
 if __name__ == '__main__':
@@ -40,4 +49,5 @@ if __name__ == '__main__':
         main()
     except Exception as e:
         traceback.print_exc()
-        input('按任意键退出')
+    finally:
+        input('按任意键退出...')
